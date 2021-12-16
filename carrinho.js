e4aa1d92d31bea5e2618b653115eaad296cdb70f
Thais Fernandes <93415279+tatafernandes@users.noreply.github.com>
2021-12-16 03:57:18 +0000

    Add files via upload

---
+++ carrinho.js
@@ -0,0 +1,208 @@
+class Background {
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = 0;
+        this.posY = 0;
+        this.score = 0;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        img.src = './images/background.jpg';
+    };
+
+    draw() {
+        this.context.globalAlpha = 0.4;
+        this.context.drawImage(this.img, this.posX, this.posY, this.canvas.width, this.canvas.height);
+    };
+};
+
+class Carrinho {
+    constructor(canvas, context, x, y, w, h) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = x;
+        this.posY = y;
+        this.width = w;
+        this.height = h;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        img.src = './images/carrinho.png';
+        this.speed = 65;
+    }
+
+    draw() {
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);
+    };
+
+    moveLeft() {
+        if (this.posX > 0) {
+        this.posX -= this.speed;
+        };
+    }
+
+    moveRight() {
+        if (this.posX < this.canvas.width - this.width) {
+            this.posX += this.speed;
+        };
+    }
+
+
+    top() {
+        return this.posY;
+    }
+
+    bottom() {
+        return this.posY + this.height;
+    }
+
+    left() {
+        return this.posX;
+    }
+
+    right() {
+        return this.posX + this.width;
+    }
+
+    checkCollision(object) {
+        if (this.posX + 80 < object.posX + object.width &&
+            this.posX + 80 + 90 > object.posX &&
+            this.posY + 150 < object.posY + object.height &&
+            this.posY + 150 + 60 > object.posY)
+        return true;
+    }
+};
+
+
+class Laco {
+    constructor(canvas, context, x, w, h) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = x;
+        this.posY = 0;
+        this.width = w;
+        this.height = h;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        const imgSrc = ['./images/laco-rosa.gif'];
+        img.src = imgSrc[Math.floor(Math.random() * imgSrc.length)];
+        this.speed = 3;
+    };
+
+    draw() {
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);  
+    };
+
+    move() {
+        this.posY += this.speed;
+    }
+
+    top() {
+        return this.posY;
+    }
+
+    bottom() {
+        return this.posY + this.height;
+    }
+
+    left() {
+        return this.posX;
+    }
+
+    right() {
+        return this.posX + this.width;
+    }
+};
+
+class Obstacles {
+    constructor(canvas, context, x, w, h) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = x;
+        this.posY = 0;
+        this.width = w;
+        this.height = h;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        const imgSrc = ['./images/laco-azul.png'];
+        img.src = imgSrc[Math.floor(Math.random() * imgSrc.length)];
+        this.speed = 3;
+    };
+
+    draw() {
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);  
+    };
+
+    move() {
+        this.posY += this.speed;
+    }
+
+    top() {
+        return this.posY;
+    }
+
+    bottom() {
+        return this.posY + this.height;
+    }
+
+    left() {
+        return this.posX;
+    }
+
+    right() {
+        return this.posX + this.width;
+    }
+};
+
+
+class Fralda {
+    constructor(canvas, context, x, w, h) {
+        this.canvas = canvas;
+        this.context = context;
+        this.posX = x;
+        this.posY = 0;
+        this.width = w;
+        this.height = h;
+        const img = new Image();
+        img.onload = () => {
+            this.img = img;
+        };
+        img.src = './images/fralda-suja.jpg';
+        this.speed = 3;
+    };
+
+    draw() {
+        this.context.globalAlpha = 1;
+        this.context.drawImage(this.img, this.posX, this.posY, this.width, this.height);  
+    };
+
+    move() {
+        this.posY += this.speed;
+    }
+
+    top() {
+        return this.posY;
+    }
+
+    bottom() {
+        return this.posY + this.height;
+    }
+
+    left() {
+        return this.posX;
+    }
+
+    right() {
+        return this.posX + this.width;
+    }
+};
---
+++ index.js
@@ -0,0 +1,200 @@
+
+  window.onload = () => {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const background = new Background(canvas, context);
+    const player = new Carrinho(canvas, context, 350, 400, 240, 300);
+    let animationId = null;
+    let frames = 0;
+    let laco = [];
+    let obstacles = [];
+    let fralda = [];
+    let powerUps = [];
+    let score = document.querySelector(".game-score");
+    let scoreNumber = document.getElementById('current-score');
+    const gameOver = new Image;
+    gameOver.src = './images/game-over.jpg';
+    let isGameOver = false;
+    let isGameRunning = false;
+    const gameWin = new Image;
+    gameWin.src = './images/its-girl.jpg';
+
+    function showScore() {
+        score.style.display = "block";
+      };
+    
+      function startGame() {
+        if (isGameRunning === false) {
+          animationId = setInterval(updateCanvas, 10);
+          document.getElementById('start-button').innerText = 'Restart Game';
+          showScore();
+          isGameRunning = true;
+        } else {
+          restartGame();
+        }
+      }
+
+      function restartGame() {
+        clearCanvas();
+        clearInterval(animationId);
+        frames = 0;
+        ingredients = [];
+        obstacles = [];
+        powerUps = [];
+        fralda = [];  
+        background.score = 0;
+        scoreNumber.innerText = background.score;
+        player.posX = 350;
+        player.posY = 400;
+        isGameOver = false;
+        isGameRunning = false;
+        startGame();
+      }
+
+      function createLaco() {
+        const min = Math.ceil(75);
+        const max = Math.floor(890);
+        const lacoX = Math.floor(Math.random() * (max - min)) + min;
+        laco.push(new Laco(canvas, context, lacoX, 40, 40));
+      }
+
+      function updateLaco() {
+        for (let i = 0; i < laco.length; i += 1) {
+          laco[i].move();
+          laco[i].draw();
+          if (laco[i].posY >= canvas.height) {
+            laco.shift();
+          };
+        };
+        if (frames % 200 === 0) {
+          createLaco();
+        }
+      }
+    
+      function checkCatchLaco() {
+        let caught = laco.some(function(eachLaco) {
+          return player.checkCollision(eachLaco);
+        });  
+        if (caught) {
+          laco.forEach((eachLaco, index) => {
+            laco.splice(index, 1);
+            background.score += 1;
+            scoreNumber.innerText = background.score;
+          });
+        };
+      }
+
+      function createObstacle() {
+        const min = Math.ceil(90);
+        const max = Math.floor(910);
+        const obstacleX = Math.floor(Math.random() * (max - min)) + min;
+        obstacles.push(new Obstacles(canvas, context, obstacleX, 50, 60));
+      }
+    
+      function updateObstacles() {
+        for (let i = 0; i < obstacles.length; i += 1) {
+          obstacles[i].move();
+          obstacles[i].draw();
+          if (obstacles[i].posY >= canvas.height) {
+            obstacles.shift();
+          };
+        };
+        if (frames % 300 === 0) {
+          createObstacle();
+        }
+      }
+    
+      function checkCatchObstacle() {
+        let caught = obstacles.some(function(obstacle) {
+          return player.checkCollision(obstacle);
+        });  
+        if (caught) {
+          obstacles.forEach((obstacle, index) => {
+            obstacles.splice(index, 1);
+            background.score -= 1;
+            scoreNumber.innerText = background.score;
+          });
+        };
+      }
+
+      function createFralda() {
+        const min = Math.ceil(90);
+        const max = Math.floor(910);
+        const fraldaX = Math.floor(Math.random() * (max - min)) + min;
+    fralda.push(new Fralda(canvas, context, fraldaX, 150, 180));
+      }
+    
+      function updateFralda() {
+        for (let i = 0; i < fralda.length; i += 1) {
+          fralda[i].move();
+          fralda[i].draw();
+          if (fralda[i].posY >= canvas.height) {
+            fralda.shift();
+          };
+        };
+        if (frames % 600 === 0) {
+          createFralda();
+        }
+      }
+
+
+      function checkGameOver() {
+        let caught = fralda.some(function(eachFralda) {
+          return player.checkCollision(eachFralda);
+        });  
+        if (caught || background.score < 0) {
+          isGameOver = true;
+          clearInterval(animationId);
+          clearCanvas();
+          context.drawImage(gameOver, 0, 0, canvas.width, canvas.height);
+        };
+      }
+    
+      function checkWin() {
+      if (background.score >= 20) {
+          clearInterval(animationId);
+          clearCanvas();
+          context.drawImage(gameWin, 0, 0, canvas.width, canvas.height);
+        };
+      }
+    
+      function clearCanvas() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    
+      function updateCanvas() {
+        frames += 1;
+        clearCanvas();
+        background.draw();
+        player.draw();
+        updateLaco();
+        checkCatchLaco();
+        updateObstacles();
+        checkCatchObstacle();
+        updateFralda();
+        checkGameOver();
+        checkWin();
+      }
+    
+      document.getElementById('start-button').onclick = () => {
+        startGame();
+        console.log("test")
+
+      };
+    
+      document.addEventListener('keydown', (event) => {
+        if (isGameOver === false && isGameRunning === true) {
+          switch (event.code) {
+            case 'ArrowLeft':
+            case 'KeyA':
+              player.moveLeft();
+              break;
+            case 'ArrowRight':
+            case 'KeyD':
+              player.moveRight();
+              break;
+          };
+          updateCanvas();
+        }
+      });
+    }
